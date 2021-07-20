533907a1ffeca32c7efb64404d8bee77dbc1241e
littlesam95 <55424662+littlesam95@users.noreply.github.com>
2021-07-20 22:10:43 +0900

    Add files via upload

---
+++ 11. 비트마스킹/Silver 5/Baekjoon1094.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+int X;
+int answer = 0;
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	cin >> X;
+	while (X > 0) {
+		if (X % 2 == 1) {
+			answer++;
+		}
+		X /= 2;
+	}
+	cout << answer << "\n";
+
+	getchar();
+	getchar();
+	return 0;
+}
