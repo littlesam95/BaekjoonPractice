7d30d1ed1d9964351c1eec84df3d84f1464775fa
littlesam95 <55424662+littlesam95@users.noreply.github.com>
2022-03-04 20:15:23 +0900

    Add files via upload

---
+++ 구현/Bronze 5/Baekjoon10926.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#define FIRST cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
+
+using namespace std;
+string ID;
+
+int main() {
+	FIRST
+	cin >> ID;
+	cout << ID + "??!" << "\n";
+
+	getchar();
+	getchar();
+	return 0;
+}
